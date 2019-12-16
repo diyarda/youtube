aff4066d1fd0736cac4705903fcbf9bc308a8794
programlama temelleri DIYARDA <58919562+diyarda@users.noreply.github.com>
2019-12-16 12:52:52 +0100

    Add files via upload

---
+++ hesap-ders2.py
@@ -0,0 +1,8 @@
+print (2+2)   #addition
+print(2-2)   #subtraktion
+print(3+3)   #int (Integer)
+print((4+8)/2)   #double, float, hier wird double ausgegeben
+print((-5-1)*3)   #negative Zahlen, positive Zahlen
+print(0*5)   #null multiplikation
+print(0/5)  #null division
+print(5/0)  #null division  error
